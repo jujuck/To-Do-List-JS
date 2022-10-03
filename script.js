ef1f26e594472afc4fb1fcd97fb76928fd37664b
Julien ichard WCS <julien.y.richard@gmail.com>
2022-10-03 10:20:06 +0200

    Creation de la variable template

--- script.js
+++ script.js
@@ -12,7 +12,15 @@ const tasksElements = document.getElementById('task');
 
 const generateTask = (tache) => {
   const tr = document.createElement('tr');
-  
+  const task = `<th scope="row" class="text-primary">1</th>
+    <th>Pain</th>
+    <th>Achat de 2 pains</th>
+    <th>Alimentaire</th>
+    <th>
+      <img src="./images/check-square.svg" alt="" class="d-none">
+      <img src="./images/square.svg" alt="">
+      <img src="./images/trash-fill.svg" alt="">
+    </th>`
 }
 
 generateTask(tasks[0]);
